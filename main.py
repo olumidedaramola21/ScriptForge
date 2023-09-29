445be43c4fea184ec43034e597a3afa57a9a4e59
Olumide Daramola <olumidedaramola426@gmail.com>
2023-09-29 17:40:44 +0100

    update

--- main.py
+++ main.py
@@ -70,10 +70,24 @@ class Pet:
 
 
 class Cat(Pet):
+    def __init__(self, name, age, color):
+        super().__init__(name, age)
+        self.color = color
+
     def speak(self):
         print("Meow")
+
+    def show(self):
+        print(f"I am {self.name} and I am {self.age} years old, I am {self.color}")
 
 
 class Dog(Pet):
     def speak(self):
         print("Bark")
+
+
+p = Pet("Tim", 19)
+p.show()
+c = Cat("Bill", 19, "Brown")
+c.show()
+c.speak()
