5faa28ee1c788929caa22bb8f465b42d2073dcde
Olumide Daramola <olumidedaramola426@gmail.com>
2023-09-29 10:52:53 +0100

    update

--- main.py
+++ main.py
@@ -1,12 +1,57 @@
-class Dog:
-    def __init__(self, name, age):
+# class Dog:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def get_name(self):
+#         return self.name
+
+#     def get_age(self):
+#         return self.age
+
+
+# dog1 = Dog("layke", 5)
+# dog2 = Dog("Jesses", 5)
+# c = dog1.get_name()
+# d = dog2.get_name()
+# print(c, d)
+# print(dog1.age)
+
+
+from os import name
+
+
+class Student:
+    def __init__(self, name, age, grade):
         self.name = name
         self.age = age
+        self.grade = grade  # 0 - 100
 
-    def get_name(self):
-        return self.name
+    def get_grade(self):
+        return self.grade
 
 
-b = Dog("layke", 5)
-c = b.get_name()
-print(c)
+class Course:
+    def __init__(self, name, max_students):
+        self.name = name
+        self.max_students = max_students
+        self.students = []
+
+    def add_student(self, student):
+        if len(self.students) < self.max_students:
+            self.students.append(student)
+            return True
+        return False
+
+    def get_average_grade(self):
+        pass
+
+
+s1 = Student("Tim", 19, 95)
+s2 = Student("Bill", 19, 75)
+s3 = Student("Jill", 19, 65)
+
+course = Course("Science", 2)
+course.add_student(s1)
+course.add_student(s2)
+print(course.students[0].name)
