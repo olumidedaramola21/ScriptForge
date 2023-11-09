2e854fd25012af6ffa523b70affb2e6fa13830da
Olumide <96479799+olumidedaramola21@users.noreply.github.com>
2023-11-09 01:02:26 +0100

    add test for setUp

--- tests/test_hw_1.py
+++ tests/test_hw_1.py
@@ -17,3 +17,11 @@ class TestMain(TestCase):
             )
         )
         self.mock_stdout = StringIO()
+
+    def test(self):
+        with patch('hw.Greeting', new=self.mock_greeting):
+            with patch('sys.stdout', new=self.mock_stdout):
+                hw.main()
+        self.mock_greeting.assert_called_with('hello', 'world')
+        self.mock_greeting.return_value.__str__.assert_called_with()
+        self.assertEqual("mock str output\n", self.mock_stdout.getvalue())
