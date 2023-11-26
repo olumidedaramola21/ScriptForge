a5ab62d555d329b1d8da6ef86bb11dfd2a16a917
Olumide <96479799+olumidedaramola21@users.noreply.github.com>
2023-11-26 13:48:57 +0100

    update

--- src/hw.py
+++ src/hw.py
@@ -40,41 +40,61 @@ for thing in examples:
     # print(f"{n} has perimeter {p:.2f} and area {a:.2f}")
 
 
+# def square_perimeter(thing):
+#     return 4 * thing["side"]
+
+# def square_area(thing):
+#     return thing["side"] ** 2
+
+# def square_new(name, side):
+#     return {
+#         "name": name,
+#         "side": side,
+#         "perimeter": square_perimeter,
+#         "area": square_area
+#     }
+
+# def circle_perimeter(thing):
+#     return 2 * math.pi * thing["radius"]
+
+# def circle_area(thing):
+#     return math.pi * thing["radius"] ** 2
+
+# def circle_new(name, radius):
+#     return {
+#         "name": name,
+#         "radius": radius,
+#         "perimeter": circle_perimeter,
+#         "area": circle_area
+#     }
+
+# # If you want to use methods in this dictionary, we call it like this
+# def call(thing, method_name):
+#     return thing[method_name](thing)
+
+# exam = [square_new("sq", 3), circle_new("ci", 2)]
+# for ex in exam:
+#     n = ex["name"]
+#     p = call(ex, "perimeter")
+#     a = call(ex, "area")
+#     print(f"{n} {p:.2f} {a:.2f}")
+
+
 def square_perimeter(thing):
     return 4 * thing["side"]
 
 def square_area(thing):
     return thing["side"] ** 2
 
+Square = {
+    "perimeter": square_perimeter,
+    "area": square_area,
+    "_classname": "Square"
+}
+
 def square_new(name, side):
     return {
         "name": name,
         "side": side,
-        "perimeter": square_perimeter,
-        "area": square_area
+        "_class": Square
     }
-
-def circle_perimeter(thing):
-    return 2 * math.pi * thing["radius"]
-
-def circle_area(thing):
-    return math.pi * thing["radius"] ** 2
-
-def circle_new(name, radius):
-    return {
-        "name": name,
-        "radius": radius,
-        "perimeter": circle_perimeter,
-        "area": circle_area
-    }
-
-# If you want to use methods in this dictionary, we call it like this
-def call(thing, method_name):
-    return thing[method_name](thing)
-
-exam = [square_new("sq", 3), circle_new("ci", 2)]
-for ex in exam:
-    n = ex["name"]
-    p = call(ex, "perimeter")
-    a = call(ex, "area")
-    print(f"{n} {p:.2f} {a:.2f}")
