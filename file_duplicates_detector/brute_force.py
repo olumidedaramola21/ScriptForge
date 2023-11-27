624909e6c8e3160c4ee49446d31cb7011cb29d06
Olumide <96479799+olumidedaramola21@users.noreply.github.com>
2023-11-27 14:24:45 +0100

    update

--- file_duplicates_detector/brute_force.py
+++ file_duplicates_detector/brute_force.py
@@ -1,17 +1,21 @@
 import sys
 import glob
+import os
 
 
 def same_bytes(left_name, right_name):
-    left_bytes = open(left_name, "rb").read()
-    right_bytes = open(right_name, "rb").read()
-    return left_bytes == right_bytes
+    with open(left_name, "rb") as left_bytes, open(right_name, "rb") as right_bytes:
+    # left_bytes = open(left_name, "rb").read()
+    # right_bytes = open(right_name, "rb").read()
+        left_content = left_bytes.read().decode('utf-8')
+        right_content = right_bytes.read().decode('utf-8')
+    return left_content == right_content
 
 def find_duplicates(filenames):
     matches = []
     for i, left in enumerate(filenames):
         for right in filenames[i + 1:]:
-            if same_bytes(left, right):
+            if same_bytes(os.path.normpath(left), os.path.normpath(right)):
                 matches.append((left, right))
     return matches
 
@@ -22,6 +26,8 @@ if __name__ == "__main__":
 
     pattern = sys.argv[1]
     filenames = glob.glob(pattern)
+    print(filenames)
+    
     
     if not filenames:
         print(f"No files found matching the pattern: {pattern}")
@@ -30,3 +36,4 @@ if __name__ == "__main__":
     duplicates = find_duplicates(filenames)
     for (left, right) in duplicates:
         print(left, right)
+
