dfda01ed4e85d970272b017e1afc4ca402be157f
Olumide <96479799+olumidedaramola21@users.noreply.github.com>
2023-11-27 10:29:04 +0100

    it works haha

--- file_duplicates_detector/brute_force.py
+++ file_duplicates_detector/brute_force.py
@@ -1,18 +1,32 @@
 import sys
+import glob
+import os
+
 def same_bytes(left_name, right_name):
     left_bytes = open(left_name, "rb").read()
     right_bytes = open(right_name, "rb").read()
     return left_bytes == right_bytes
 
-def find_duplicates(filename):
+def find_duplicates(filenames):
     matches = []
-    for left in filename:
-        for right in filename:
+    for i, left in enumerate(filenames):
+        for right in filenames[i + 1:]:
             if same_bytes(left, right):
                 matches.append((left, right))
     return matches
 
 if __name__ == "__main__":
-    duplicates = find_duplicates(sys.argv[1:])
+    if len(sys.argv) < 2:
+        print("Usage: python brute_force.py <pattern>")
+        sys.exit(1)
+
+    pattern = sys.argv[1]
+    filenames = glob.glob(pattern)
+    
+    if not filenames:
+        print(f"No files found matching the pattern: {pattern}")
+        sys.exit(1)
+
+    duplicates = find_duplicates(filenames)
     for (left, right) in duplicates:
         print(left, right)
