384090935864e0af5583590ae83b208c7de98a6c
Olumide <96479799+olumidedaramola21@users.noreply.github.com>
2023-12-09 10:30:12 +0100

    added print statement to show how the operation happen

--- pattern_matching/glob_lit.py
+++ pattern_matching/glob_lit.py
@@ -2,11 +2,20 @@ class Lit:
     def __init__(self, chars, rest=None):
         self.chars = chars
         self.rest = rest
+        # print(f"chars: {chars}, rest: {rest}")
 
     def match(self, text, start=0):
         end = start + len(self.chars)
+        # print(f"start: {start}, end: {end}, text: {text}")
         if text[start:end] != self.chars:
+            # print(text[start:end], self.chars)
             return False
         if self.rest:
+            # print(f"self.rest: {self.rest}")
             return self.rest.match(text, end)
         return end == len(text)
+    
+pattern = Lit("a", Lit("b"))
+result = pattern.match("ab")
+
+print(f"Final result: {result}")
